873f922e45ccfc1521182d53adeaf7ec1f435b0b
Kyle Noyes <Kylesnoyes@gmail.com>
2026-05-21 11:39:13 -0600

    Add student courses endpoint and methods

--- main.py
+++ main.py
@@ -5,15 +5,30 @@ from starlette.status import HTTP_404_NOT_FOUND
 
 from database import get_db
 from models import Student, Instructor, Course
-from schemas import CreateStudentRequest, CreateStudentResponse, CreateInstructorRequest, CreateInstructorResponse, CreateCourseRequest, CreateCourseResponse
-
+from schemas import (
+    CreateStudentRequest,
+    CreateStudentResponse,
+    CreateInstructorRequest,
+    CreateInstructorResponse,
+    CreateCourseRequest,
+    CreateCourseResponse,
+)
 
 app = FastAPI()
 
-#students
+
+# students
 @app.get("/students")
 async def get_students(db: Session = Depends(get_db)) -> list[Student]:
     return db.exec(select(Student)).all()
+
+
+@app.get("/students/{student_id}/courses")
+async def get_student_courses(student_id: int, db: Session = Depends(get_db)) -> dict[str, str]:
+    student: Student | None = db.get(Student, student_id)
+    if student is None:
+        raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found!")
+    return {course.course_number: course.title for course in student.courses}
 
 
 @app.post("/students", status_code=status.HTTP_201_CREATED)
@@ -22,6 +37,18 @@ async def create_student(new_student: CreateStudentRequest, db: Session = Depend
     db.add(student)
     db.commit()
     return CreateStudentResponse(student_id=student.student_id)
+
+
+@app.post("/students/{student_id}", status_code=status.HTTP_201_CREATED)
+async def add_student_to_course(student_id: int, course_id: int, db: Session = Depends(get_db)) -> None:
+    student: Student | None = db.get(Student, student_id)
+    if student is None:
+        raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found!")
+    course: Course | None = db.get(Course, course_id)
+    if course is None:
+        raise HTTPException(status_code=404, detail=f"Course with ID {course_id} not found!")
+    student.courses.append(course)
+    db.commit()
 
 
 @app.delete("/students/{student_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -33,7 +60,7 @@ async def delete_student(student_id: int, db: Session = Depends(get_db)):
     db.commit()
 
 
-#instructors
+# instructors
 @app.get("/instructors")
 async def get_instructors(db: Session = Depends(get_db)) -> list[Instructor]:
     return db.exec(select(Instructor)).all()
@@ -45,10 +72,10 @@ async def get_instructors_courses(id: int, db: Session = Depends(get_db)) -> dic
     if instructor is None:
         raise HTTPException(status_code=404, detail=f"Instructor with ID {id} not found!")
     return {course.course_number: course.title for course in instructor.courses}
-    #course_names: list[str] = []
-    #for i in range(len(instructor.courses)):
+    # course_names: list[str] = []
+    # for i in range(len(instructor.courses)):
     #    course_names.append(instructor.courses[i].title)
-    #return course_names
+    # return course_names
 
 
 @app.get("/instructors/{id}/num-courses")
@@ -60,7 +87,9 @@ async def get_number_of_courses(id: int, db: Session = Depends(get_db)) -> int:
 
 
 @app.post("/instructors", status_code=status.HTTP_201_CREATED)
-async def create_instructor(new_instructor: CreateInstructorRequest, db: Session = Depends(get_db)) -> CreateInstructorResponse:
+async def create_instructor(
+    new_instructor: CreateInstructorRequest, db: Session = Depends(get_db)
+) -> CreateInstructorResponse:
     instructor = Instructor(**new_instructor.model_dump())
     db.add(instructor)
     db.commit()
@@ -76,7 +105,7 @@ async def delete_instructor(id: int, db: Session = Depends(get_db)):
     db.commit()
 
 
-#courses
+# courses
 @app.get("/courses")
 async def get_courses(db: Session = Depends(get_db)) -> list[Course]:
     return db.exec(select(Course)).all()
--- models.py
+++ models.py
@@ -5,12 +5,15 @@ class StudentCourseLink(SQLModel, table=True):
     student_id: int = Field(primary_key=True, foreign_key="students.student_id")
     course_id: int = Field(primary_key=True, foreign_key="courses.course_id")
 
+
 class Student(SQLModel, table=True):
     __tablename__ = "students"
     student_id: int | None = Field(default=None, primary_key=True)
     first_name: str
     last_name: str
     email: str
+    courses: list[Course] = Relationship(back_populates="students", link_model=StudentCourseLink)
+
 
 class Instructor(SQLModel, table=True):
     __tablename__ = "instructors"
@@ -20,6 +23,7 @@ class Instructor(SQLModel, table=True):
     email: str
     courses: list[Course] = Relationship(back_populates="instructor")
 
+
 class Course(SQLModel, table=True):
     __tablename__ = "courses"
     course_id: int | None = Field(default=None, primary_key=True)
@@ -28,3 +32,4 @@ class Course(SQLModel, table=True):
     title: str
     course_number: str
     credits: int
+    students: list[Student] = Relationship(back_populates="courses", link_model=StudentCourseLink)
